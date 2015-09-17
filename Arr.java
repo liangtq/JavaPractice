10e28e1bb84059cecf9c6a33a3dd20ec3822ff09
liangtq <liang_tian_qi@live.cn>
2015-09-17 17:55:53 -0500

    Day4

---
+++ Arr.java
@@ -0,0 +1,24 @@
+public class Arr
+{
+	public static void main(String[] args)
+	{
+		int[] arr = new int[]{1,4,125,3,6,8,28};
+		int a = comp(arr);
+		System.out.println(a);
+		
+	}
+	
+	public static int comp(int[] arr)
+	{
+		int max = arr[0];
+		for(int a=1; a<arr.length;a++)
+		{
+			if( arr[a]>max)
+			{
+				max=arr[a];
+			}
+			
+		}
+		return max;
+	}
+}
--- Test.java
+++ Test.java
@@ -2,7 +2,20 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-			System.out.println("Hello world");
+			int[] shuzu = new int[] {1,3,4,5,2,6};
+			printArr(shuzu);
+	}
+	
+	public static void printArr(int[] arr)
+	{
+		System.out.print("[");
+		for(int a=0;a<arr.length;a++)
+		{
+			if(a!=arr.length-1)
+			 System.out.print(arr[a]+", ");
+		 	else
+		 	 System.out.print(arr[a]+"] \n");
+		}
 	}
 
 }
