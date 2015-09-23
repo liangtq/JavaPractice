ed1363125913fde4766cd689df23aca0a495a0e0
liangtq <liang_tian_qi@live.cn>
2015-09-23 11:13:22 -0500

    Day5
    object oriented programming

---
+++ Day5/CarDemo.java
@@ -0,0 +1,29 @@
+/*
+描述汽车：汽车的 属性+行为
+*/
+
+class Car
+{
+	//属性就是class中的变量
+	String color = "red";
+	int tire = 4;
+	//行为就是class中的函数（方法）
+	void run()
+	{
+		System.out.println("color is "+color+"tire is "+tire);
+	}
+}
+
+class CarDemo
+{
+	public static void main(String[] args)
+	{
+		//生产汽车
+		Car c = new Car();
+		c.color = "blue";
+		
+		Car c1 = new Car();
+		c1.run();
+		
+	}
+}
