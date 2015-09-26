2d75a8c26b4620ba25e0ada5bd2957306d79ecdc
liangtq <liang_tian_qi@live.cn>
2015-09-26 17:50:39 -0500

    Day5
    this

--- Day5/PersonDemo1.java
+++ Day5/PersonDemo1.java
@@ -9,12 +9,21 @@ this代表本类对象，到底代表哪一个呢？
 	简单说：哪个对象在调用this所在函数，this就代表哪个对象，this可能是p或者p1。
 	变量有同名情况时候，必须加this
 	类（class）里成员被使用，全是由对象来完成，本类里的对象是this，this是运行的对象
+this的应用：
+	当定义类中功能时，该函数内部要用到调用该函数的对象时，这时用this来表示这个对象。谁后期调用我，我就代表谁
+	但凡本类功能内部使用到了本类对象，那么都用this表示
+this就是代表对象，找对象。
 */
 
 class Person
 {
 	private String name;
 	private int age;
+	
+	Person(int age)
+	{
+		this.age = age;
+	}
 	
 	Person(String name)
 	{
@@ -39,17 +48,26 @@ class Person
 	{
 		System.out.println(this.name);
 	}
-	
+	/*
+	需求：给人定义一个用于比较年龄是否相同的功能，也就是是否是同龄人。
+	*/
+	public boolean ageComp(Person p)//人与人比，不是人与年龄比
+	{
+		//if(this.age == p.age ) //this是p1，因为p1在调用这个函数，p接收的是p2的值
+			return (this.age == p.age);
+	}
 }
 
 class PersonDemo1
 {
 	public static void main(String[] args)
 	{
-		Person p = new Person("lisi");
-		Person p1 = new Person("zhangsan");
-		p.speak();
-		p1.speak();
+		Person p1 = new Person(20);
+		Person p2 = new Person(50);
+		boolean b = p1.ageComp(p2);
+		System.out.println(b);
+		//p.speak();
+		//p1.speak();
 		
 	}
 }
